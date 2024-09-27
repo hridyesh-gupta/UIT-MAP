ad7ebb81730368b275f5f38e9be33dc7824c7ed4
sarthak singh <147877697+SarthakSingh2003@users.noreply.github.com>
2024-09-27 10:27:16 +0530

    Update guidelines.php
    ADDED THE RUBRICS SAMPLE AT THE END

--- guidelines.php
+++ guidelines.php
@@ -173,7 +173,86 @@ elseif($_SESSION['usertype'] == "mentor"){ //If the user is mentor show the ment
             <h4 class="text-md font-semibold mb-2">Rubrics for evaluation of B. Tech. Projects:</h4>
             <p class="text-justify">To maintain uniform evaluation criteria, the DEC has framed project evaluation rubrics, which will be used for evaluating the projects. The project evaluation rubrics will be shared with all the supervisors and students at the beginning of the project work.</p>
         </section>
+        <section class="mb-8">
+            <center><h2 class="text-2xl font-bold mb-4">Rubrics Review</h2></center>
+            <table class="w-full border-collapse">
+                <thead>
+                    <tr class="bg-gray-200">
+                        <th class="border px-4 py-2">Review #</th>
+                        <th class="border px-4 py-2">Agenda</th>
+                        <th class="border px-4 py-2">Assessment</th>
+                        <th class="border px-4 py-2">Last Date</th>
+                        <th class="border px-4 py-2">Review Assessment Weightage</th>
+                        <th class="border px-4 py-2">Overall Weightage</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="hover:bg-blue-50">
+                        <td class="border px-4 py-2">Review 1</td>
+                        <td class="border px-4 py-2">Project Proposal Evaluation</td>
+                        <td class="border px-4 py-2">Rubric R1</td>
+                        <td class="border px-4 py-2">March 15, 2023</td>
+                        <td class="border px-4 py-2">(18)</td>
+                        <td class="border px-4 py-2" rowspan="3">50</td>
+                    </tr>
+                    <tr class="hover:bg-blue-50">
+                        <td class="border px-4 py-2">Review 2</td>
+                        <td class="border px-4 py-2">Project Synopsis</td>
+                        <td class="border px-4 py-2">Rubric R2</td>
+                        <td class="border px-4 py-2">May 30, 2023</td>
+                        <td class="border px-4 py-2">(24)</td>
+                    </tr>
+                    <tr class="hover:bg-blue-50">
+                        <td class="border px-4 py-2">Review 3</td>
+                        <td class="border px-4 py-2">Evaluation by Supervisor</td>
+                        <td class="border px-4 py-2">Rubric R3</td>
+                        <td class="border px-4 py-2">July 25, 2023</td>
+                        <td class="border px-4 py-2">(8)</td>
+                    </tr>
+                    <tr class="hover:bg-blue-50">
+                        <td class="border px-4 py-2">Review 4</td>
+                        <td class="border px-4 py-2">7th Semester Project Evaluation</td>
+                        <td class="border px-4 py-2">Rubric R4</td>
+                        <td class="border px-4 py-2">August 30, 2023 *</td>
+                        <td class="border px-4 py-2">(100)</td>
+                        <td class="border px-4 py-2">100</td>
+                    </tr>
+                    <tr class="hover:bg-blue-50">
+                        <td class="border px-4 py-2">Review 5</td>
+                        <td class="border px-4 py-2">8th Semester Project Evaluation</td>
+                        <td class="border px-4 py-2">Rubric R5</td>
+                        <td class="border px-4 py-2">November 15, 2023</td>
+                        <td class="border px-4 py-2">(50(I)+100(E)=150)</td>
+                        <td class="border px-4 py-2" rowspan="4">400</td>
+                    </tr>
+                    <tr class="hover:bg-blue-50">
+                        <td class="border px-4 py-2">Review 6</td>
+                        <td class="border px-4 py-2">Project Report Evaluation</td>
+                        <td class="border px-4 py-2">Rubric R6</td>
+                        <td class="border px-4 py-2">December 30, 2023</td>
+                        <td class="border px-4 py-2">(30(I)+60(E)=90)</td>
+                    </tr>
+                    <tr class="hover:bg-blue-50">
+                        <td class="border px-4 py-2">Review 7</td>
+                        <td class="border px-4 py-2">Evaluation by Department Project Coordinator</td>
+                        <td class="border px-4 py-2">Rubric R7</td>
+                        <td class="border px-4 py-2">March 30, 2024</td>
+                        <td class="border px-4 py-2">(20(I)+50(E)=70)</td>
+                    </tr>
+                    <tr class="hover:bg-blue-50">
+                        <td class="border px-4 py-2">Review 8</td>
+                        <td class="border px-4 py-2">Evaluation by Supervisor</td>
+                        <td class="border px-4 py-2">Rubric R8</td>
+                        <td class="border px-4 py-2">April 6, 2024</td>
+                        <td class="border px-4 py-2">90</td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" class="border px-4 py-2 font-bold">Total</td>
+                        <td class="border px-4 py-2 font-bold">550</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
     </main>
-
 </body>
 </html>
