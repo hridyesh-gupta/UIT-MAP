a7074694caae327905445d5a483879c24d5bbb7c
hridyesh-gupta <hridyesh.gupta1206@gmail.com>
2024-10-20 11:35:24 +0530

    Minor change

--- groups.php
+++ groups.php
@@ -4,7 +4,7 @@ session_start();
 if(!(isset($_SESSION['username']))){  //If the session variable is not set, then it means the user is not logged in and is accessing this page through url editing, as we have provided session username to every user who logged in. So, redirecting to login page
     header("location: index.php");
 }
-elseif($_SESSION['usertype']!="admin" && $_SESSION['usertype']!="mentor"){ //If the user is not admin or mentor, then it means the user is student and is accessing this page through url editing as we have provided admin usertype to every user who logged in via admin credentials. So, redirecting to login page
+elseif($_SESSION['usertype']!="admin"){ //If the user is not admin, then it means the user is student and is accessing this page through url editing as we have provided admin usertype to every user who logged in via admin credentials. So, redirecting to login page
     header("location: index.php");
 }
 
