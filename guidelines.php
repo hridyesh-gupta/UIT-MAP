e31bbdbe48d05b7356ce73f2fd7aa664ec7f3168
hridyesh-gupta <hridyesh.gupta1206@gmail.com>
2025-01-04 16:21:20 +0530

    Added spinner after each save button

--- guidelines.php
+++ guidelines.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+error_reporting(0);
 if(!(isset($_SESSION['username']))){  //If the session variable is not set, then it means the user is not logged in and is accessing this page through url editing, as we have provided session username to every user who logged in. So, redirecting to login page
     header("location: index.php");
 }
